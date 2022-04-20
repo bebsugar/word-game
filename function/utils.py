42d8a7088628ed1ae99d98cbc2dc51ca6c81d64f
Ezgi Ersoy <eeezgiersoy@gmail.com>
2022-04-20 20:17:59 +0300

    add util functions

--- function/utils.py
+++ function/utils.py
@@ -1,4 +1,4 @@
-from db.db import cursor
+from db.db import cursor, mydb
 
 
 def get_random_word():
@@ -7,3 +7,30 @@ def get_random_word():
     random_word = cursor.fetchone()
     return random_word
 
+
+def check_date_exists(date) -> bool:
+    query = "SELECT date FROM statistics WHERE date = %s"
+    cursor.execute(query, [date])
+    row = cursor.fetchone()
+    if row is None:
+        return False
+    return True
+
+
+def insert_row(date, total_count, correct_answer_count):
+    sql = "INSERT INTO statistics (date, total_answer, correct_answers) VALUES (%s, %s, %s)"
+    val = (date, total_count, correct_answer_count)
+    cursor.execute(sql, val)
+    mydb.commit()
+
+
+def update_row(total_count, correct_answer_count, date):
+    sql = """
+    UPDATE statistics
+    SET total_answer = total_answer + %s,
+    correct_answers = correct_answers + %s
+    WHERE date = %s;
+    """
+    val = (total_count, correct_answer_count, date)
+    cursor.execute(sql, val)
+    mydb.commit()
