a63b98a6e4436e0c660059416d8fa30a0e1c7943
Ezgi Ersoy <eeezgiersoy@gmail.com>
2022-04-20 20:18:11 +0300

    update rows if already exists

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from function.utils import get_random_word
+from function.utils import get_random_word, check_date_exists, insert_row, update_row
+from datetime import datetime
+
+total_count = 0
+correct_answer_count = 0
+date = datetime.today().strftime('%Y-%m-%d')
 
 
 def main():
@@ -8,10 +13,22 @@ def main():
     guess = input(f'{word}: ')
     if guess == translation:
         print('Correct!')
-        main()
+        global correct_answer_count
+        correct_answer_count += 1
+
     else:
         print(f'Incorrect. The translation of the word: {translation}')
+    global total_count
+    total_count += 1
+
+    reply = str(input('Do you want a new word? (y/n) '))
+    if reply == 'y':
         main()
+    else:
+        if check_date_exists(date):
+            update_row(total_count, correct_answer_count, date)
+        else:
+            insert_row(date, total_count, correct_answer_count)
 
 
 main()
